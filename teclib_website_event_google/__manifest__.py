a6a49d027fafbb7714114299c798eb0eea4f6dc2
Khalid BENTALEB <khalid.bentaleb@captivea.com>
2022-02-23 16:18:35 +0100

    Google Event module

---
+++ teclib_website_event_google/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Teclib Website Event Google',
+    'version': '1.1',
+    'author': 'Teclib Group',
+    'description': """
+    This module makes events indexed by Google.
+    """,
+    'category': 'Marketing/Events',
+    'website': 'https://www.teclib-group.com/',
+    'depends': [
+        'website_event',
+    ],
+    'data': [
+        'views/website_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
